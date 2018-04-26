8ec65d578a01aca4cdbc8bb8aadf41f3e1a23be9
DESKTOP-K9PBG7V\Nahian <nahian.csedu@gmail.com>
2018-04-26 04:05:27 -0400

    Build dictionary of vocabulary

--- HRNNSequentialImageDescription.py
+++ HRNNSequentialImageDescription.py
@@ -14,36 +14,42 @@ class SequentialImageDescription:
     def __init__(self):
         self.image_features = dict()
         self.annotations = dict()
-    def loadImageFeatures(self):
-        image_features_list = pickle.load(open(StoredImageFeaturesFile, 'rb'))
-        for j in range(len(image_features_list)):
-            image_id = image_features_list[j]['id']
-            features = image_features_list[j]['features']
-            self.image_features[image_id] = features
+
+def loadImageFeatures():
+    image_features_list = pickle.load(open(StoredImageFeaturesFile, 'rb'))
+    image_features = dict()
+    for j in range(len(image_features_list)):
+        image_id = image_features_list[j]['id']
+        features = image_features_list[j]['features']
+        image_features[image_id] = features
     
-    def loadAnnotations(self):
-        description_metadata = json.load(open(ImageDescriptionMetaDataFile))
-        annotations = description_metadata['annotations']
-        description = dict()
-        annotations_len = len(annotations)
+    return image_features
     
-        for i in range(annotations_len):
-            for j in range(len(annotations[i])):
-                photo_id = annotations[i][j]['photo_flickr_id']
-                if photo_id in self.image_features.keys():
-                    image_des = annotations[i][j]['original_text']
-                    image_des = image_des.lower()
-                    image_des = image_des.strip()
-                    image_des = image_des.replace(',', ' ,')
-                    image_des = image_des.replace('.', '')
-                    image_des = image_des.replace('"', ' " ')
-                    description[photo_id] = image_des
+def loadAnnotations(image_features):
+    description_metadata = json.load(open(ImageDescriptionMetaDataFile))
+    annotations = description_metadata['annotations']
+    description = dict()
+    annotations_len = len(annotations)
+    
+    for i in range(annotations_len):
+        for j in range(len(annotations[i])):
+            photo_id = annotations[i][j]['photo_flickr_id']
+            if photo_id in image_features.keys():
+                image_des = annotations[i][j]['original_text']
+                image_des = image_des.lower()
+                image_des = image_des.strip()
+                image_des = image_des.replace(',', ' ,')
+                image_des = image_des.replace('.', '')
+                image_des = image_des.replace('"', ' " ')
+                description[photo_id] = image_des
+    
+    return description
                     
     
 def main():
-    sqnImgDesc = SequentialImageDescription()
-    sqnImgDesc.loadImageFeatures()
-    sqnImgDesc.loadAnnotations()()
+    
+    image_features = loadImageFeatures()
+    annotations = loadAnnotations(image_features)
 
 if __name__ == "__main__":
     main()
